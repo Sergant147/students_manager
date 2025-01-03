e95da743d890a8bf361863ff5e3c50099bcede31
duhov <portoslunt@gmail.com>
2025-01-03 21:59:18 +0300

    draft

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,21 @@
 class Student {
-    public String NameID;
-    public String information;
+    private String NameID;
+    private String information;
 
     public Student(String name, String information) {
         this.NameID = name;
         this.information = information;
+    }
+
+    public String getInf() {
+        return this.information;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setInformation(String name) {
+        this.name = name;
     }
 }
