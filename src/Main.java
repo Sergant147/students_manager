2e805efa07fa0b3cdf3ff9629c816faf54059a2d
duhov <portoslunt@gmail.com>
2025-01-03 22:45:16 +0300

    draft

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package programs;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Students {
+public class Mains {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Student> studentsArray = new ArrayList<Student>();
