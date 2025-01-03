6473dd3d5f2943ed295ca89547c85c26d6d542ca
duhov <portoslunt@gmail.com>
2025-01-03 21:55:28 +0300

    draft

--- src/StudentCommandHandler.java
+++ src/StudentCommandHandler.java
@@ -1,30 +1,36 @@
-public class StudentCommandHanndler {
-    public StudentCommandHanndler(){}
-    public static void showStudent(Student student){
-        System.out.println("Student "+student.NameID);
-        System.out.println(student.information);
-        System.out.println();
-    }
-    public static void showStudents(ArrayList<Student> students){
-        for (Student student: students){
-            showStudent(student);
+    public class StudentCommandHanndler {
+        public StudentCommandHanndler() {
         }
-    }
-    public static ArrayList<Student> removeStudent(ArrayList<Student> students, String name){
-        students.removeIf(student -> student.NameID.equals(name));
-        return students;
-    }
-    public static ArrayList<Student> addStudent(ArrayList<Student> students, String name, String inf){
-        Student newOne = new Student(name, inf);
-        students.add(newOne);
-        return students;
-    }
-    public static ArrayList<Student> editStudent(ArrayList<Student> students, String name, String inf){
-        for (Student student: students){
-            if (student.NameID.equals(name)) {
-                student.information = inf;
+
+        public static void showStudent(Student student) {
+            System.out.println("Student " + student.NameID);
+            System.out.println(student.information);
+            System.out.println();
+        }
+
+        public static void showStudents(ArrayList<Student> students) {
+            for (Student student : students) {
+                showStudent(student);
             }
         }
-        return students;
+
+        public static ArrayList<Student> removeStudent(ArrayList<Student> students, String name) {
+            students.removeIf(student -> student.NameID.equals(name));
+            return students;
+        }
+
+        public static ArrayList<Student> addStudent(ArrayList<Student> students, String name, String inf) {
+            Student newOne = new Student(name, inf);
+            students.add(newOne);
+            return students;
+        }
+
+        public static ArrayList<Student> editStudent(ArrayList<Student> students, String name, String inf) {
+            for (Student student : students) {
+                if (student.NameID.equals(name)) {
+                    student.information = inf;
+                }
+            }
+            return students;
+        }
     }
-}
--- src/Students.java
+++ src/Students.java
@@ -1,6 +1,8 @@
 package programs;
+
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Students {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,43 +11,37 @@ public class Students {
         String name;
         String information;
         StudentsCommandHandler studentsHandler = new StudentsCommandHandler();
-        while (true){
+        while (true) {
             System.out.print("Command (add, edit, remove, get_all, get, stop, clear): ");
             command = scanner.nextLine();
-            if (command.equals("stop")){
+            if (command.equals("stop")) {
                 break;
-            }
-            else if (command.equals("add")){
+            } else if (command.equals("add")) {
                 System.out.print("Name: ");
                 name = scanner.nextLine();
                 System.out.print("Information: ");
                 information = scanner.nextLine();
                 studentsArray = studentsHandler.addStudent(studentsArray, name, information);
-            }
-            else if (command.equals("remove")){
+            } else if (command.equals("remove")) {
                 System.out.print("Name: ");
                 name = scanner.nextLine();
                 studentsArray = studentsHandler.removeStudent(studentsArray, name);
-            }
-            else if (command.equals("edit")){
+            } else if (command.equals("edit")) {
                 System.out.print("Name: ");
                 name = scanner.nextLine();
                 System.out.print("Information: ");
                 information = scanner.nextLine();
                 studentsArray = studentsHandler.editStudent(studentsArray, name, information);
-            }
-            else if (command.equals("get_all")){
-               studentsHandler.showStudents(studentsArray);
-            }
-            else if (command.equals("clear")){
+            } else if (command.equals("get_all")) {
+                studentsHandler.showStudents(studentsArray);
+            } else if (command.equals("clear")) {
                 studentsArray = new ArrayList<Student>();
-            }
-            else {
+            } else {
                 System.out.print("Name: ");
                 name = scanner.nextLine();
-                for (Student student: studentsArray){
-                    if (student.NameID.equals(name)){
-                       studentsHandler.showStudent(student);
+                for (Student student : studentsArray) {
+                    if (student.NameID.equals(name)) {
+                        studentsHandler.showStudent(student);
                     }
                 }
             }
